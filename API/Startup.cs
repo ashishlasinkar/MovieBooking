234b011fb61e6dcd54f1126ec737d411ca825c71
ashishlasinkar <ashishlasinkar@gmail.com>
2021-08-16 17:56:45 +0530

    adding CORS and calling get movies API

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,7 +49,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseEndpoints(endpoints =>
             {
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'client';
+  movies: any;
+
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    this.getMovies();
+  }
+
+  getMovies() {
+    this.http.get('https://localhost:5001/movies').subscribe(
+      (response) => {
+        this.movies = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
